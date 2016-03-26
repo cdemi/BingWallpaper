5a43e5e3b270dbe9c206ad967a8a26cdb43919ef
Christopher Demicoli <christopher.demicoli@gaminginnovationgroup.com>
2016-03-26 16:58:18 +0100

    Application

---
+++ BingWallpaper/Program.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading;
+
+namespace BingWallpaper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isConnected = false;
+            while (!isConnected)
+            {
+                try
+                {
+                    Thread.Sleep(5 * 1000);
+                    BingImage bingResponse;
+                    string imageURL;
+                    using (WebClient bingClient = new WebClient())
+                    {
+                        bingResponse = JsonConvert.DeserializeObject<BingImage>(bingClient.DownloadString("http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US"));
+                        isConnected = true;
+                        imageURL = $"http://www.bing.com{bingResponse.images.FirstOrDefault().url}";
+                    }
+                    string wallpapersPath = $@"{Environment.GetFolderPath(Environment.SpecialFolder.MyPictures)}\Bing Wallpapers\";
+                    string picturePath = $"{wallpapersPath}{bingResponse.images.FirstOrDefault().hsh}.jpg";
+                    if (!File.Exists(picturePath))
+                    {
+                        if (!Directory.Exists(wallpapersPath))
+                            Directory.CreateDirectory(wallpapersPath);
+                        using (WebClient imageClient = new WebClient())
+                        {
+                            imageClient.DownloadFile(imageURL, picturePath);
+                        }
+                        Wallpaper.Set(picturePath);
+                    }
+                }
+                catch (WebException)
+                {
+                    Thread.Sleep(5 * 1000);
+                }
+                catch (Exception)
+                {
+                }
+            }
+        }
+    }
+}
---
+++ BingWallpaper/Wallpaper.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BingWallpaper
+{
+    public sealed class Wallpaper
+    {
+        Wallpaper() { }
+
+        const int SPI_SETDESKWALLPAPER = 20;
+        const int SPIF_UPDATEINIFILE = 0x01;
+        const int SPIF_SENDWININICHANGE = 0x02;
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+
+        public static void Set(string picturePath)
+        {
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Control Panel\Desktop", true);
+            key.SetValue(@"WallpaperStyle", 10.ToString());
+            key.SetValue(@"TileWallpaper", 0.ToString());
+
+            SystemParametersInfo(SPI_SETDESKWALLPAPER,
+                0,
+                picturePath,
+                SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
+        }
+    }
+}
